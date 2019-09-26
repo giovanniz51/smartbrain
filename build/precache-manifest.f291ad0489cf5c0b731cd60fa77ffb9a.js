5e8a70018d20eb7acbf50f04f23457703f30d347
EC2 Default User <ec2-user@ip-172-31-13-33.eu-west-1.compute.internal>
2019-09-26 13:20:46 +0000

    build prod

---
+++ build/precache-manifest.f291ad0489cf5c0b731cd60fa77ffb9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f3926837c2c449955338c4654929b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8123e89f125b6f75fa8c",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4ad4b1564a5e3dde0e84",
+    "url": "/static/css/main.55f2431e.chunk.css"
+  },
+  {
+    "revision": "8123e89f125b6f75fa8c",
+    "url": "/static/js/2.ce4afcfb.chunk.js"
+  },
+  {
+    "revision": "4ad4b1564a5e3dde0e84",
+    "url": "/static/js/main.b949e9c7.chunk.js"
+  },
+  {
+    "revision": "0fec360040d3993f74de",
+    "url": "/static/js/runtime-main.600a8fe0.js"
+  }
+]);
